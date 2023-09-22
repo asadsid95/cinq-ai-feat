0011637f5f80d01b38c9bd5044d4df4449e3656c
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-09-22 01:36:43 -0400

    sidebar added

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
     return (
         <div>
             <p>Dashboard page (protected)</p>
-            <UserButton afterSignOutUrl="/" />
         </div>
 
     )
