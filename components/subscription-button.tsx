df8d48f869b68e4198d8fece193932bddf60966f
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-09-25 03:10:46 -0400

    missing import for toast

--- components/subscription-button.tsx
+++ components/subscription-button.tsx
@@ -4,6 +4,7 @@ import { Zap } from "lucide-react"
 import { Button } from "./ui/button"
 import axios from "axios"
 import { useState } from "react"
+import toast from "react-hot-toast"
 
 interface SubscriptionButtonProps {
     isPro: boolean
